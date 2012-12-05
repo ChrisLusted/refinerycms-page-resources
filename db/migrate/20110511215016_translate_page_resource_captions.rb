772c4733b0a05f7ae7cc15f545f7f2a2f6899e9b
ChrisLusted <chris@chrislusted.co.uk>
2012-12-05 10:26:10 +0000

    Update db/migrate/20110511215016_translate_page_resource_captions.rb
    Remove add ID column as this is not needed

--- db/migrate/20110511215016_translate_page_resource_captions.rb
+++ db/migrate/20110511215016_translate_page_resource_captions.rb
@@ -1,7 +1,5 @@
 class TranslatePageResourceCaptions < ActiveRecord::Migration
   def up
-    add_column :refinery_page_resources, :id, :primary_key
-
     Refinery::PageResource.reset_column_information
     unless defined?(Refinery::PageResource::Translation) && Refinery::PageResource::Translation.table_exists?
       Refinery::PageResource.create_translation_table!({
@@ -16,7 +14,5 @@ class TranslatePageResourceCaptions < ActiveRecord::Migration
     Refinery::PageResource.reset_column_information
 
     Refinery::PageResource.drop_translation_table! :migrate_data => true
-
-    remove_column Refinery::PageResource.table_name, :id
   end
 end
